e0decb8662cac55ce1204a50b3badb01827a85ec
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-07-30 09:54:18 +0700

    feat:complete algo skill checkpoint

--- question-2.js
+++ question-2.js
@@ -1,5 +1,18 @@
 function findProductPrice(products, name) {
-  // Your code here
+  let left = 0;
+  let right = products.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2 );
+    if (products[mid].name === name) {
+      return products[mid].price
+    } else if (products[mid].name < name) {
+      left = mid + 1;
+    } else {
+      right = mid - 1
+    }
+  }
+  return -1 ;
 }
 
 // Test case
@@ -8,6 +21,7 @@ const products = [
   { name: "Banana", price: 0.8 },
   { name: "Cherry", price: 2.5 },
 ];
+
 
 console.log(findProductPrice(products, "Banana")); // Output: 0.8
 console.log(findProductPrice(products, "Durian")); // Output: -1
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer: O n กำลัง 2 เนื่องจากมีการลูป 2 ครั้ง ทำให้จำนวนนั้นเพิ่มขึ้นเท่าตัว 
 
 
 */
